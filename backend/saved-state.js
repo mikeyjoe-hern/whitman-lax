e6395d4786d7160c376d916f0712868527f1c541
Mike Hernandez <mike.joe.hern@gmail.com>
2026-03-05 11:37:09 -0500

    Save practice plan state

--- backend/saved-state.js
+++ backend/saved-state.js
@@ -217,7 +217,7 @@ window.SAVED_STATE = {
       "dateValue": "2026-03-05",
       "dateDisplay": "Thursday, March 5, 2026",
       "coachNotes": "Compete/Game Like Situtation, Hard Hat Day 4 (Lukas)",
-      "overallNote": "GRADES, RUN TEST MONDAY, TO DO TMO- LIVE FACEOFFS, RIDE/CLEAR",
+      "overallNote": "GRADES, RUN TEST MONDAY, TO DO TMO- LIVE FACEOFFS, RIDE/CLEAR, HELMET HANDOUT TMO",
       "focusText": "PREP",
       "pquote": "\"You may delay, but time will not\" - Benjamin Franklin"
     }
